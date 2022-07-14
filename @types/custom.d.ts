768092846f3af64fbf186313a94d01e34a6e2752
Seolhun <shun10114@gmail.com>
2022-07-14 23:15:48 +0900

    chore: add resources type declration files

---
+++ @types/custom.d.ts
@@ -0,0 +1,30 @@
+declare module '*.ico' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Record<string, any>;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: Record<string, any>;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+
+declare module '*.md' {
+  const content: string;
+  export default content;
+}
