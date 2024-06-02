9c4b50759c44de242a4bfb6ff473706909ef6bd6
Seolhun <shun10114@gmail.com>
2024-06-03 06:55:36 +0900

    refactor: migrate all editor to playground codes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
 const { resolve } = require('path');
-const context = resolve(__dirname, '../src');
+const { merge } = require("webpack-merge");
+const commonConfig = require("../build/webpack.common");
 
 export default {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  staticDirs: ['../public', '../src/images'],
+  staticDirs: ['../public', '../src/assets'],
 
   features: {
     previewMdx2: true, // 👈 MDX 2 enabled here
@@ -17,46 +18,13 @@ export default {
     '@chromatic-com/storybook'
   ],
 
+  /**
+   * @param {import('webpack').Configuration} config
+   */
   webpackFinal: async (config) => {
-    config.resolve.alias = {
-      '~': context,
-    }
-    config.context = context;
-    config.module.rules.push(
-      {
-        test: [/\.[jt]sx?$/],
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-      // {
-      //   test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-      //   type: 'asset/resource',
-      //   generator: {
-      //     filename: 'assets/[name][ext]',
-      //   },
-      // },
-      {
-        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/[name].[ext]',
-            },
-          },
-        ],
-      }
-    )
-    return config;
+    return merge(commonConfig, config, {
+
+    });
   },
 
   framework: {
