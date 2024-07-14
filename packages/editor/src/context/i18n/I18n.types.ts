27b51e4e5fe75722eef8e779617243c6a5143cd6
Seolhun <shun10114@gmail.com>
2024-07-15 04:57:08 +0900

    refactor: rename i18n to lowercase

--- packages/editor/src/context/i18n/I18n.types.ts
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react';
-
-export type I18nMessages = Record<string, React.ReactNode>;
-export type I18nResources = Record<string, I18nMessages>;
-export type I18nTFunction = (key: string) => ReactNode;
--- packages/editor/src/context/i18n/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './I18nContext';
-export * from './I18n.types';
-export * from './I18n.resources';
