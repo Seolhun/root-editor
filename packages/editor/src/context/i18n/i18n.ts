97bfe3c2e9b7f734e80be62429b571896dc3f0ab
Seolhun <shun10114@gmail.com>
2024-09-03 14:04:30 +0900

    fix: fix the i18n global references error affecting application i18n instance

--- packages/editor/src/context/i18n/i18n.ts
+++ packages/editor/src/context/i18n/i18n.ts
@@ -1,10 +1,11 @@
-import i18n from 'i18next';
+import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import { DEFAULT_I18N_RESOURCES } from './i18n.resources';
 import { i18nResource } from './i18n.types';
 
 export function createI18n(language: string, resources?: i18nResource) {
+  const i18n = i18next.createInstance();
   i18n.use(initReactI18next).init({
     fallbackLng: language,
     interpolation: {
